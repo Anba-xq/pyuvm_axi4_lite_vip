2525222225f18ea9421ee6349994b3632259d03c
Anba-xq <124248020+Anba-xq@users.noreply.github.com>
2026-04-29 18:43:53 +0800

    Update axi_lite_scoreboard.py

--- vip/axi_lite_scoreboard.py
+++ vip/axi_lite_scoreboard.py
@@ -15,12 +15,13 @@ class AxiScoreboard(uvm_scoreboard):
             self.logger.debug(f"寄存器加载: 地址 {hex(addr)} -> 复位值 {hex(val)}")
         while True:
             item = await self.fifo.get()
+            aligned_addr = item.addr & 0xFFFFFFFC
             if item.is_write:
-                self.memory_map[item.addr] = item.data
-                self.logger.info(f"💾 [SCB] 记录写入 -> 寄存器 {hex(item.addr)} 更新为 {hex(item.data)}")
+                self.memory_map[aligned_addr] = item.data
+                self.logger.info(f"💾 [SCB] 记录写入 -> 寄存器 {hex(item.addr)}(映射到物理地址 {hex(aligned_addr)}) 更新为 {hex(item.data)}")
             else:
-                expected = self.memory_map.get(item.addr, 0)
+                expected = self.memory_map.get(aligned_addr, 0)
                 if item.data == expected:
-                    self.logger.info(f"✅ [SCB] 比对成功! 地址 {hex(item.addr)}: 期望={hex(expected)}, 实际={hex(item.data)}")
+                    self.logger.info(f"✅ [SCB] 比对成功! 地址 {hex(aligned_addr)}: 期望={hex(expected)}, 实际={hex(item.data)}")
                 else:
-                    self.logger.error(f"❌ [SCB] 比对失败! 地址 {hex(item.addr)}: 期望={hex(expected)}, 实际={hex(item.data)}")
+                    self.logger.error(f"❌ [SCB] 比对失败! 地址 {hex(aligned_addr)}: 期望={hex(expected)}, 实际={hex(item.data)}")
